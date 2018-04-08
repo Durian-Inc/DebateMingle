54418fe5e67af2cdd35d749c31d400980041eb12
Innocent Niyibizi <25cent9@gmail.com>
2018-04-08 01:26:52 -0500

    Docs: Added documentation for each model. Each description has a small blurb and a list of the attributes

--- debatemingle/models.py
+++ debatemingle/models.py
@@ -6,7 +6,12 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 class Account(db.Model):
-    
+    """Account for each individual.
+
+    Attributes:
+        username: The user's unique username.
+        password: The password that the user has entered but in a hashed form.
+    """
     __tablename__ = "account"
 
     username = db.Column(db.String, nullable = False, unique = True, primary_key = True)
@@ -17,10 +22,14 @@ class Account(db.Model):
         self.password = password
 
     def __repr__(self):
-
         return '<Account %r>' % (self.username)
 
 class Topic(db.Model):
+    """Collection of topics that are in the datanase. 
+
+    Attributes:
+        name: The unique name of the topic.
+    """
     __tablename__ = "topic"
 
     name = db.Column(db.String(80), primary_key = True)
@@ -32,6 +41,13 @@ class Topic(db.Model):
         return '<Topic %r>' % (self.name)
         
 class VotedOn(db.Model):
+    """Model to hold to correlate each user with how they feel for each topic. 
+
+    Attributes:
+        person: The name of the person for a given entry.
+        topic: The topic at hand.
+        vote: The user's opinion of the topic. 
+    """
     __tablename__ = "votedon"
     person = db.Column('person', db.String, db.ForeignKey('account.username'), primary_key = True)
     topic = db.Column('topic', db.String, db.ForeignKey('topic.name'), primary_key = True)
